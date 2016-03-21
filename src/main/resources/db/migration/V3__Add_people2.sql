36e89fec7b545fa9ba01995580298d407c559bac
Nicola Bertazzo <nicola.bertazzo@gmail.com>
2016-03-21 15:40:00 +0100

    Squashed commit of the following:
    commit 466f03a8755f946bb3cfff0c6f36e3701aaa81e6
    Author: Nicola Bertazzo <nicola.bertazzo@gmail.com>
    Date:   Mon Mar 21 15:19:49 2016 +0100
    
    	aggiunto angelo
    
    commit 1c673625c67f5db5ca2efd6aa6f8adf333465b3d
    Author: Nicola Bertazzo <nicola.bertazzo@gmail.com>
    Date:   Mon Mar 21 15:18:42 2016 +0100
    
    	aggiunte persone

---
+++ src/main/resources/db/migration/V3__Add_people2.sql
@@ -0,0 +1,4 @@
+insert into PERSON (ID, NAME) values (4, 'Nicola');
+insert into PERSON (ID, NAME) values (5, 'Daniele');
+insert into PERSON (ID, NAME) values (6, 'Matteo');
+insert into PERSON (ID, NAME) values (6, 'Angelo');
